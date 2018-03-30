13475b135e0c53cd2e0487c7bb3e1e9118a809c4
Roman Collaud <roman.collaud@swisscom.com>
2018-03-31 01:13:24 +0200

    header integration

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import banner from './asset/banner.svg';
+import photo from './asset/photo-profil.jpg';
 import './Header.css';
 
 class Header extends Component {
   render() {
     return (
         <div className="App-header">
-          <img src={banner} className="App-banner" alt="banner"/>
+          <div className="banner">
+            <img src={banner} className="App-banner" alt="banner"/>
+          </div>
+          <div className="profil-container">
+            <h2>Roman Collaud</h2>
+            <h3>Apprenti 3ème année Swisscom SA</h3>
+          </div>
+          <div className="photo-container">
+            <img src={photo} className="photo-cv" alt="profil"/>
+          </div>
         </div>
     );
   }
